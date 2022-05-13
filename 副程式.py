7514fe5f513ac4d00ad747d4fc5286c188eed70c
SkyWei8905 <91265925+SkyWei8905@users.noreply.github.com>
2022-05-13 20:32:13 +0800

    Add files via upload
    副程式

---
+++ 副程式.py
@@ -0,0 +1,35 @@
+'''
+def Sum(x):
+    a = 0
+    for i in range(1,x+1):  
+        print (i)
+
+Sum(10)
+'''
+
+
+
+
+'''
+def Sum(x):
+    a = 0
+    for i in range(1,x+1):
+        a+=i
+    return a
+
+z = Sum(10)
+print(z)
+'''
+
+
+
+'''
+def Sum(x):
+    a = 1
+    for i in range(1,x+1):
+        a*=i
+    return a
+
+z = Sum(10)
+print(z)
+'''
