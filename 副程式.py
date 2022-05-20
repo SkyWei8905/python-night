a613720610140cd42ab1efabd55b1ca49fe374b5
SkyWei8905 <91265925+SkyWei8905@users.noreply.github.com>
2022-05-20 20:19:25 +0800

    Add files via upload
    upload

--- 副程式.py
+++ 副程式.py
@@ -20,11 +20,8 @@ def Sum(x):
 z = Sum(10)
 print(z)
 '''
-
-
-
 '''
-def Sum(x):
+def mul(x):
     a = 1
     for i in range(1,x+1):
         a*=i
@@ -33,3 +30,80 @@ def Sum(x):
 z = Sum(10)
 print(z)
 '''
+
+'''
+def printStr(name, title='瓜'):
+    print(f'name={name}, title={title}')
+    
+    
+def calc(w, h):
+    return (w+h)*2, w*h    
+
+
+print(printStr(('哈')))
+
+y,z = calc(3, 4)
+'''
+
+
+'''
+s = [(3, 4),(2, 4),(5,3)]
+
+def calc(w, h):
+    return w * h
+def calcAll(conta, func):
+    for r in conta:
+        print(func(r[0], r[1]), end=' ')  #這是註解哦~
+       
+calcAll(s, calc)
+'''
+
+'''
+def Sum(x):
+    a = 0
+    for i in range(1,x+1):
+        a+=i
+    return a
+
+def mul(x):
+    a = 1
+    for i in range(1,x+1):
+        a*=i
+    return a
+
+def calcAll(value, func):
+    return func(value)
+
+
+print(calcAll(10, Sum))
+print(calcAll(10, mul))
+'''
+
+'''
+def calc(w, *args):
+    for arg in args:
+        w = w * arg
+    return w
+
+y = calc(3, 4, 5, 6)
+print(y)    
+'''
+def calc2(w, **kwargs):
+    print (w, kwargs)
+
+calc2('chris', a=1, b=2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
